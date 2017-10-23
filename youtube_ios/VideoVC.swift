3368c16e2e8616e534284ffbae7d61d5012d8dd2
Farhan Rahman <farhanrahman@Fuads-MacBook-Air.local>
2017-10-23 09:48:27 -0400

    Working on rendering a table of the videos

--- youtube_ios/VideoVC.swift
+++ youtube_ios/VideoVC.swift
@@ -10,23 +10,120 @@ import UIKit
 import Alamofire
 
 
-class VideoVC: UIViewController {
+class VideoTableViewCell : UITableViewCell {
+    
+    
+    
+}
+
+class VideoVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     //Query label
     @IBOutlet weak var queryLabel: UILabel!
     
+    //Outlet that connects the table for the videos.
+    @IBOutlet weak var allVideos: UITableView!
+    
     //Query for the search.
     var search_query : String?
     
+    //Sample videos to loop from.
+    var sample_vids = ["Video 1", "Video Two", "Video 3"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Make the table delegate itself to this class.
+        allVideos.delegate = self
         
+        // Tables datasource goes here.
+        allVideos.dataSource = self
+        
+        var API_KEY = "AIzaSyCjQGG6FSax58c2VZsDNducwsswZCz0MNM"
+        
+        var base_url = "https://www.googleapis.com/youtube/v3"
+        var search_endpoint = "/search?&q=\(search_query!)&type=video&key=\(API_KEY)&part=snippet"
+        
+        var full_url = base_url + search_endpoint
+        
+        
+        
+        //Change the query label text.
         queryLabel.text = "Query: \(search_query!)"
         
-        // Do any additional setup after loading the view.
+        
+        Alamofire.request(full_url).responseJSON { response in
+           
+            
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+            
+        
+//            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+//                print("Data: \(utf8Text)") // original server data as UTF8 string
+//            }
+        
+        
+        }
+        
+        
+        
     }
+    
+    // Function that returns the amount of cells in the application.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sample_vids.count
+    }
+    
+    // Function that gives the number of sections in the application.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+// OLD FUNCTION
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cellIdentifier = "videoCell"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? UITableViewCell  else {
+            fatalError("The dequeued cell is not an instance of UITableViewCell")
+        }
+        
+        let meal = sample_vids[indexPath.row]
+
+        print(cell)
+        return cell
+    
+    }
+    
+
+
+    
+//    //MARK: - Tableview Delegate & Datasource
+//    func tableView(_ tableView:UITableView, numberOfRowsInSection section:Int) -> Int
+//    {
+//        return 10
+//    }
+//    
+//    
+//    func numberOfSectionsInTableView(tableView: UITableView) -> Int
+//    {
+//        return 1
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+//    {
+//        
+//        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "MyTestCell")
+//        return cell
+//    }
+//    
+//    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
+//    {
+//        
+//    }
 
 
     
